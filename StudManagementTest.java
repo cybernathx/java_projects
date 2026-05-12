4a7fdfdef0e331cd8e17b996f371eddd42de1aab
cybernathx <cybernathx@gmail.com>
2026-05-12 20:20:31 +0100

    Initial Commit: Student Management Project

---
+++ StudManagementTest.java
@@ -0,0 +1,123 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class StudManagementTest {
+    public static void main(String[] args) {
+        
+        ArrayList<StudManagement> students = new ArrayList<>();
+        Scanner input = new Scanner(System.in);
+
+        int ID;
+        String names;
+        int ages;
+        String depts;
+        int count, choice, deleteID, searchID;
+
+        while (true) {
+            System.out.println("\nWelcome! What do you want to do?\n1.Add Student\n2.View all Students\n3.Search\n4.Delete Student\n5.Exit");
+            System.out.print("\nSelect an option: ");
+            
+            choice = input.nextInt();
+            input.nextLine();
+
+            if (choice == 1) {
+                System.out.print("How many students do you want to add?: ");
+                count = input.nextInt();
+                input.nextLine();
+
+                for (int i = 0; i < count; i++) {
+
+                    System.out.println("\nStudent" + (i + 1));
+
+                    System.out.print("Input student id: ");
+                    ID = input.nextInt();
+                    input.nextLine();
+
+                    System.out.print("Input student name: ");
+                    names = input.nextLine();
+
+                    System.out.print("Input student age: ");
+                    ages = input.nextInt();
+                    input.nextLine();
+
+                    System.out.print("Input student department: ");
+                    depts = input.nextLine();
+                
+                    students.add(new StudManagement(ID, names, ages, depts));
+
+                    System.out.println("\nAdded Successfully!");                   
+                }
+            } else if (choice == 2) {
+                if (students.isEmpty()) {
+                    System.out.println("Oops!, Empty record");
+                } else {
+                    System.out.println("\n----ALL STUDENTS----");
+                
+
+                    for (StudManagement s : students) {
+                        System.out.println("ID: " + s.getID());
+                        System.out.println("Name : " + s.getName());
+                        System.out.println("Age: " + s.getAge());
+                        System.out.println("Course: " + s.getCourse());
+                        System.out.println();
+                    }
+                }
+            } else if (choice == 3) {
+                if (students.isEmpty()) {
+                    System.out.println("\n---RECORD IS EMPTY!---");
+                } else {
+                    System.out.print("Who do you want to search for (by ID): ");
+                    searchID = input.nextInt();
+                    input.nextLine();
+
+                    for (int i = 0; i < students.size(); i++) {
+                        boolean found = false;
+                        if (searchID == students.get(i).getID()) {
+                            System.out.println("\nStudent ID: " + students.get(i).getID());
+                            System.out.println("Student Name: " + students.get(i).getName());
+                            System.out.println("Student Age: " + students.get(i).getAge());
+                            System.out.println("Student Course: " + students.get(i).getCourse());
+                            found = true;
+                        } else if (!found) {
+                            System.out.println("\n---RECORD NOT FOUND!---");
+                        } 
+                    }
+                }
+            } else if (choice == 4){
+                if (students.isEmpty()) {
+                    System.out.println("\n---RECORD IS EMPTY---!");
+                } else {
+                    System.out.print("\nInput id of student you want to delete: ");
+                    deleteID = input.nextInt();
+                    input.nextLine();
+
+                    boolean found = false;
+
+                    for (int i = 0; i < students.size(); i++) {
+
+                        if (deleteID == students.get(i).getID()) {
+                            System.out.println("Are you sure you want to delete " + deleteID + "? (y/n)");
+                            String ans = input.nextLine();
+                            if (ans.equals("y")) {
+                                students.remove(i);
+                                found = true;
+                                break;
+                            } else {
+                                break;
+                            }
+                                    
+                        }
+
+                        if (!found) {
+                            System.out.println("---NOT FOUND!---");
+                        }
+                    }
+                }
+            } else if (choice == 5) {
+                break;
+            }
+             
+        }
+         input.close();
+    }
+}
